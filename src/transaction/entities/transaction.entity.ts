c78fb215e4acdf2b66b5ba3c9821dccd6392a94d
pdtung03 <TungPDHE173492@fpt.edu.vn>
2024-11-25 21:51:49 +0700

    update transaction table

--- src/transaction/entities/transaction.entity.ts
+++ src/transaction/entities/transaction.entity.ts
@@ -24,7 +24,7 @@ export class Transaction {
   @Column({ nullable: false })
   status: boolean;
 
-  @ManyToOne(() => Order, order => order.orderId)
+  @ManyToOne(() => Order, (order) => order.orderId, { nullable: true })
   @JoinColumn({ name: 'orderId' })
   order: Order;
 
@@ -36,4 +36,48 @@ export class Transaction {
   @JoinColumn({ name: 'receiveUserId' })
   receiveUser: User;
 
+}
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Order } from '../../order/entities/order.entity';
+import { User } from '../../user/entities/user.entity';
+@Entity()
+export class Transaction {
+  @PrimaryGeneratedColumn()
+  transactionId: number;
+
+  @Column({ nullable: false })
+  transferUserId: number;
+
+  @Column({ nullable: false })
+  receiveUserId: number;
+
+  @Column({ nullable: false })
+  transactionDate: Date;
+
+  @Column('decimal', { precision: 10, scale: 2 })
+  transactionAmount: number;
+
+  @Column({ length: 100 })
+  transactionDescription: string;
+
+  @Column({ nullable: false })
+  status: boolean;
+
+  @ManyToOne(() => Order, (order) => order.orderId, { nullable: true })
+  @JoinColumn({ name: 'orderId' })
+  order: Order;
+
+  @ManyToOne(() => User, (user) => user.id)
+  @JoinColumn({ name: 'transferUserId' })
+  transferUser: User;
+
+  @ManyToOne(() => User, (user) => user.id)
+  @JoinColumn({ name: 'receiveUserId' })
+  receiveUser: User;
 }
